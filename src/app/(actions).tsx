fd5c2870e7809340af5a5cbc2c4e6122ea3fcf1e
shaik-zeeshan <shaikzeeshan999@gmail.com>
2024-02-04 21:51:27 +0530

    Add Loading to other pages

--- src/app/(actions).tsx
+++ src/app/(actions).tsx
@@ -5,65 +5,65 @@ import { UpdateUserAnimeProgress } from "@/graphql/mutation";
 import { UserAnimeProgress } from "@/graphql/user";
 import { anilist_client } from "@/lib/graphql-request";
 import { revalidatePath } from "next/cache";
-import { cookies, headers } from "next/headers";
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function logout() {
-    cookies().delete("access_token");
-    cookies().delete("user_id");
+  cookies().delete("access_token");
+  cookies().delete("user_id");
 }
 
 export async function getUserProgress(id: number) {
-    const user_id = cookies().get("user_id")?.value;
+  const user_id = cookies().get("user_id")?.value;
 
-    if (!user_id) redirect("/login");
+  if (!user_id) redirect("/login");
 
-    let res;
+  let res;
 
-    try {
-        res = await anilist_client.request(UserAnimeProgress, {
-            id,
-            userId: parseInt(user_id),
-        });
-    } catch (e) {
-        return null;
-    }
+  try {
+    res = await anilist_client.request(UserAnimeProgress, {
+      id,
+      userId: parseInt(user_id),
+    });
+  } catch (e) {
+    return null;
+  }
 
-    return res.MediaList;
+  return res.MediaList;
 }
 
 export async function updateUserProgress(data: FormData) {
-    const { id, progress, score, status } = Object.fromEntries(
-        data.entries(),
-    ) as {
-        [x: string]: string;
-    };
+  const { id, progress, score, status } = Object.fromEntries(
+    data.entries(),
+  ) as {
+    [x: string]: string;
+  };
 
-    if (!parseInt(id)) return null;
+  if (!parseInt(id)) return null;
 
-    const access_token = cookies().get("access_token")?.value;
+  const access_token = cookies().get("access_token")?.value;
 
-    if (!access_token) redirect("/login");
+  if (!access_token) redirect("/login");
 
-    try {
-        await anilist_client.request(
-            UpdateUserAnimeProgress,
-            {
-                mediaId: parseInt(id),
-                ...(progress && { progress: parseFloat(progress) as number }),
-                ...(score && { score: parseInt(score) as number }),
-                ...(status && { status: status as MediaListStatus }),
-            },
-            {
-                Authorization: `Bearer ${access_token}`,
-            },
-        );
-    } catch (e) {
-        console.log(e);
+  try {
+    await anilist_client.request(
+      UpdateUserAnimeProgress,
+      {
+        mediaId: parseInt(id),
+        ...(progress && { progress: parseFloat(progress) as number }),
+        ...(score && { score: parseInt(score) as number }),
+        ...(status && { status: status as MediaListStatus }),
+      },
+      {
+        Authorization: `Bearer ${access_token}`,
+      },
+    );
+  } catch (e) {
+    console.log(e);
 
-        return null;
-    }
+    return null;
+  }
 
-    revalidatePath(`/anime/${id}`);
-    revalidatePath(`/manga/${id}`);
+  revalidatePath(`/anime/${id}`);
+  revalidatePath(`/manga/${id}`);
 }
---
+++ src/app/(media)/loading.tsx
@@ -0,0 +1,9 @@
+import { Loader } from "@/components/loader";
+
+export default function Page() {
+  return (
+    <main className="flex h-full w-full flex-col items-center justify-center">
+      <Loader />
+    </main>
+  );
+}
